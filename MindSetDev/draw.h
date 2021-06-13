f0c56a5457861ca72d7d9d5822077f9d006499f6
Varitina <70086724+Varitina@users.noreply.github.com>
2021-06-13 19:56:27 +0800

    修改了函数的顺序
    在按下第一个按钮之后会莫名闪退qwq

---
+++ MindSetDev/draw.h
@@ -0,0 +1,7 @@
+// 全局变量
+extern double winwidth=15, winheight=10;// 窗口尺寸
+extern int switch_button=0;//响应mode 切换
+extern int isEditing=0;// 判断有没有在编辑文本 
+extern double mouse_x=0;
+extern double mouse_y=0;
+
